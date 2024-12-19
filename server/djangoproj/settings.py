878823e20fe7503c38020127aa597c3a238bf8b6
jxsaw <jxsaw@hotmail.com>
2024-12-20 02:01:11 +0800

    completed module 1

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = [] ###### TO CHANGE TO ACTUAL LAB'S HOSTED SITE ADDRESS WHEN OPEN IN VIRTUAL LAB #####
+CSRF_TRUSTED_ORIGINS = [] ###### TO CHANGE TO ACTUAL LAB'S HOSTED SITE ADDRESS WHEN OPEN IN VIRTUAL LAB #####
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,5 +136,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static')
+]
 
